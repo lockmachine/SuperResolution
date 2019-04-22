0d8cb5d4bb2835cb73804f9eb2d7e8f1b9e3fcaa
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-04-23 00:01:45 +0900

    Keras版SRCNNの構築ひとまず完了（教師データのサイズが合わない）

--- srcnn_ks.py
+++ srcnn_ks.py
@@ -4,11 +4,14 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.core import Activation, Flatten
-from keras.optimizers import Momentum
+from keras.optimizers import SGD
 from keras.callbacks import TensorBoard, ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
+import cv2
+import glob
+import pickle
 
-def network(input_shape):
+def SRCNN_ks(input_shape):
     model = Sequential()
     
     # SRCNN ネットワークの構成
@@ -21,6 +24,62 @@ def network(input_shape):
     
 class SRDataset():
     def __init__(self):
+        # 学習用画像サイズ
         self.image_shape = (33, 33, 3)
         
     def get_batch(self):
+        # 訓練データの読み込み
+        with open('./LR_split_data.pkl', 'rb') as f:
+            x_train_img = pickle.load(f)
+        with open('./HR_split_data.pkl', 'rb') as f:
+            t_train_img = pickle.load(f)
+            
+        x_train_img = x_train_img.astype('float32')
+        x_train_img /= 255
+        t_train_img = x_train_img.astype('float32')
+        t_train_img /= 255
+        
+        return x_train_img, t_train_img
+        
+
+class Trainer():
+    def __init__(self, model, loss, optimizer):
+        self._target = model
+        self._target.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+        self.verbose = 1
+        self.log_dir = os.path.join(os.path.dirname(__file__), 'logdir')
+        self.model_file_name = 'srcnn_model.hdf5'
+        
+    def train(self, x_train, t_train, batch_size, epochs, validation_split):
+        #if os.path.exists(self.log_dir):
+            #import shutil
+            #shutil.rmtree(self.log_dir)
+        #os.mkdir(self.log_dir)
+        
+        self._target.fit(
+            x_train, t_train,
+            batch_size=batch_size, epochs=epochs,
+            validation_split=validation_split,
+            callbacks=[
+                TensorBoard(log_dir=self.log_dir),
+                ModelCheckpoint(os.path.join(self.log_dir, self.model_file_name), save_best_only=True)
+            ],
+            verbose=self.verbose
+        )
+
+# データセットの読み込み
+dataset = SRDataset()
+
+# ネットワークのインスタンス生成
+model = SRCNN_ks(dataset.image_shape)
+
+# オプティマイザーのインスタンス生成
+optimizer = SGD(lr=0.01, momentum=0.9)
+
+x_train, t_train = dataset.get_batch()
+#print(x_train.shape)    # (4364, 33, 33, 3)
+#print(t_train.shape)    # (4364, 21, 21, 3)
+trainer = Trainer(model, loss='mean_squared_error', optimizer=optimizer)
+trainer.train(x_train, t_train, batch_size=128, epochs=12, validation_split=0.0)
+
+#score = model.evaluate()
